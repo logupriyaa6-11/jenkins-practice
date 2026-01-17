91f46e4c242449ee6e043edb45b44cc9b6d01e0d
Logupriya <priyaarul1107@gmail.com>
2026-01-17 12:49:39 +0530

    Day-5 task remoting-removed user_name password ip address

--- Week1/Day5/remoteConnection.ps1
+++ Week1/Day5/remoteConnection.ps1
@@ -2,9 +2,9 @@ Set-Item WSMan:\localhost\Client\TrustedHosts -Value "135.235.169.49" -Force
 
 Get-Item WSMan:\localhost\Client\TrustedHosts
 
-$remoteIP = "135.235.169.49"
-$username = "test_user"
-$password = "TestUser@1234"
+$remoteIP = "IP address"
+$username = "user_name"
+$password = "password"
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
