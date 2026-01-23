020d76bffdae1ddb5d475b567e65f62ad84e4ff6
Logupriya <priyaarul1107@gmail.com>
2026-01-23 09:26:31 +0530

    Rectified the commands

--- Week2/Week2-Day1/printingAlternateNumbers.sh
+++ Week2/Week2-Day1/printingAlternateNumbers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Enter elements"
-read -a array
+printAlternateNumbers() {
+    array=("$@")
+    for ((element = 0; element < ${#array[@]}; element+=2))
+    do
+        echo "${array[element]} "
+    done
+    echo
+}
 
-for ((element = 0; element < ${#array[@]}; element+=2))
-do
-    echo -n "${array[element]} "
-done
+printAlternateNumbers "$@"
